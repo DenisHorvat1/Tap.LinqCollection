b6aba9b5e56e55a4ad663a090fc36ea5ecdecf67
DenisHorvat1 <denishorvat2002@gmail.com>
2023-03-23 17:59:15 +0200

    Ex 3

---
+++ Tap.LinqCollection/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tap.LinqCollection
+{
+    internal class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public bool IsElectric { get; set; }
+        public int MaxSpeedKmh { get; set; }
+        public int AutonomyKm { get; set; }
+
+
+    }
+}
