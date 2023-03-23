48332a0da4443fbef496ece2863829116b86bb74
MariusChiarEl <marius.enache2001@gmail.com>
2023-03-23 18:33:30 +0200

    Created a CarService class (ex. 5).

---
+++ Tap.LinqCollection/CarService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tap.LinqCollection
+{
+    internal class CarService
+    {
+        CarRepository carRepository;
+
+        public CarService(CarRepository carRepository)
+        {
+            this.carRepository = carRepository;
+        }
+    }
+}
